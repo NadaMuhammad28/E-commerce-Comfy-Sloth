cc866073d6925f702ecfed2677675a3e8ca3c6a6
Nadamuhammad28 <nadamuhammad28@gmail.com>
2023-02-02 03:14:25 +0200

    add to cart for single product done

---
+++ src/components/products/AddToCart.jsx
@@ -0,0 +1,51 @@
+import Colors from "../products/singleProduct/colors";
+import Quantity from "../products/singleProduct/productQuantity";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useCartContext } from "../../context/CartContext";
+const AddtoCart = ({ product }) => {
+  const { addToCart } = useCartContext();
+  const [clrIndex, setClrIndex] = useState(0);
+  const [amount, setAmount] = useState(1);
+  const increaseAmount = () => {
+    if (amount < product.stock) {
+      setAmount(amount + 1);
+    }
+  };
+  const decreaseAmount = () => {
+    if (amount > 1) {
+      setAmount(amount - 1);
+    }
+  };
+  const addedProduct = {
+    id: product.id,
+    color: product.colors[clrIndex],
+    idColor: `${product.id}${product.colors[clrIndex]}`,
+    name: product.name,
+    image: product.images[0].url,
+    amount,
+    price: product.price,
+    subtotal: product.price * amount,
+    stock: product.stock,
+    category: product.category,
+    shipping: product.shipping,
+  };
+  return (
+    <div>
+      <Colors
+        colors={product.colors}
+        setClrIndex={setClrIndex}
+        clrIndex={clrIndex}
+      />
+
+      <Quantity
+        amount={amount}
+        increaseAmount={increaseAmount}
+        decreaseAmount={decreaseAmount}
+      />
+      <Link onClick={() => addToCart(addedProduct)}>Add to cart</Link>
+    </div>
+  );
+};
+
+export default AddtoCart;
