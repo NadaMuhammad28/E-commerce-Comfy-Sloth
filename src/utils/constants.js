dd4194120ac0b9c1f1a04b0a36a1ca2e92e91821
Nadamuhammad28 <nadamuhammad28@gmail.com>
2023-02-04 04:13:23 +0200

    const

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -18,6 +18,27 @@ export const links = [
   },
 ];
 
+export const services = [
+  {
+    id: 1,
+    Icon: GiCompass,
+    title: "mission",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+  },
+  {
+    id: 2,
+    Icon: GiDiamondHard,
+    title: "vision",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+  },
+  {
+    id: 3,
+    Icon: GiStabbedNote,
+    title: "history",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+  },
+];
+
 export const products_url = "https://course-api.com/react-store-products";
 
 export const single_product_url = `https://course-api.com/react-store-single-product?id=`;
