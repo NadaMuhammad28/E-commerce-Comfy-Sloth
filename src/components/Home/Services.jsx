35178d4dc6b7e1afdffc42f125c72e23ddb0efb2
Nadamuhammad28 <nadamuhammad28@gmail.com>
2023-02-04 16:03:38 +0200

    home

--- src/components/Home/Services.jsx
+++ src/components/Home/Services.jsx
@@ -2,20 +2,31 @@ import Grid2 from "@mui/material/Unstable_Grid2";
 import { services } from "../../utils/constants";
 import AOS from "aos";
 import "aos/dist/aos.css";
+
+import { useEffect } from "react";
 const Services = () => {
+  useEffect(() => {
+    AOS.init({ duration: 800 });
+  }, []);
   return (
-    <section className="services-sec sec-wrapper">
+    <section className="services-sec ">
       <div className="container ">
         <div className="serv-sec-wrapper">
           <header>
-            <Grid2 container spacing={{ xs: 3, sm: 1, md: 2 }}>
+            <Grid2 container spacing={{ xs: 3 }}>
               <Grid2 item md={6}>
                 <h3>
                   custom furniture <br />
                   built only for you
                 </h3>
               </Grid2>
-              <Grid2 item md={6}>
+              <Grid2
+                item
+                md={6}
+                container
+                alignItems="center"
+                justifyContent="center"
+              >
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
                   quisquam saepe id reiciendis sunt, repudiandae libero amet rem
@@ -28,11 +39,15 @@ const Services = () => {
           <div className="serv-cards-wrapper">
             <Grid2 container spacing={{ xs: 3, md: 4 }}>
               {services &&
-                services.map((card) => {
+                services.map((card, index) => {
                   let { Icon, title, text, id } = card;
                   return (
                     <Grid2 item md={4} key={id}>
-                      <article className="serv-card" data-aos="fade-up">
+                      <article
+                        className="serv-card"
+                        data-aos="fade-up"
+                        data-aos-delay={index * 10 + 100}
+                      >
                         <span className="serv-icon">
                           <Icon />
                         </span>
