36b7d1a3124952b64cc6e45c04e8822f28431548
Nadamuhammad28 <nadamuhammad28@gmail.com>
2023-02-05 14:49:29 +0200

    products reducer

--- src/reducers/poductsReducr.js
+++ src/reducers/poductsReducr.js
@@ -3,6 +3,10 @@ import {
   FETCH_DATA,
   END_FETCHING,
   PRODUCTS_ERR_FETCHING,
+  SINGLE_PRODUCT_START_FETCHING,
+  FETCH_SINGLE_PRODUCT,
+  SINGLE_PRODUCT_END_FETCHING,
+  SINGLE_PRODUCT_ERR,
 } from "../utils/actions";
 
 const reducer = (state, action) => {
@@ -33,6 +37,32 @@ const reducer = (state, action) => {
       ...state,
       productsErr: true,
     };
+  }
+
+  /////////////////////////////////////////////////////////////
+  //SINGLE PRODUCT
+  if (action.type === SINGLE_PRODUCT_START_FETCHING) {
+    return {
+      ...state,
+      issingleProductLoading: true,
+      singleProductErr: false,
+    };
+  }
+  if (action.type === FETCH_SINGLE_PRODUCT) {
+    return {
+      ...state,
+      singleProduct: action.payload,
+    };
+  }
+  if (action.type === SINGLE_PRODUCT_END_FETCHING) {
+    return {
+      ...state,
+      issingleProductLoading: false,
+    };
+  }
+
+  if (action.type == SINGLE_PRODUCT_ERR) {
+    return { ...state, singleProductErr: true, issingleProductLoading: false };
   }
   throw new Error(`No Matching "${action.type}" - action type`);
 };
